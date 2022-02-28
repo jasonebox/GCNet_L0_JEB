45b4791f52d34981faf919fa1cbec1b6b695cfbe
jason box <jason@Macbook-Pro.lan>
2022-02-28 10:26:16 +0100

    update

--- AWS_L0_JEB.py
+++ AWS_L0_JEB.py
@@ -54,6 +54,7 @@ from glob import glob
 
 files = sorted(glob("./L0_raw/08_*.dat"), reverse=False)
 
+#%%
 i=0
 dfx=pd.read_csv(files[i],names=df_names.varnam,delim_whitespace=True)
 dfx['date']=pd.to_datetime(dfx['JulianDay'], format='%j').dt.strftime('%Y-%m-%d %H')
@@ -61,7 +62,8 @@ dfx['date'] = pd.to_datetime(dfx.date) + pd.offsets.DateOffset(years=dfx.year[0]
 
 pdList = []
 
-for i,file in enumerate(files):
+# for i,file in enumerate(files):
+for i,file in enumerate(files[0:4]):
     dfx=pd.read_csv(files[i],names=df_names.varnam,delim_whitespace=True)
     dfx[dfx<-990]=np.nan
     dfx[dfx==999]=np.nan
@@ -102,9 +104,9 @@ for sensor_level in range(1,3):
 #%% apply speed of sound correction to sonic height sensor data
 for sensor_level in range(1,3):
     print(sensor_level)
-    df['kk']=df['AirTemp'+str(sensor_level)].astype(float)+273.15
-    df['tr']=(df['kk']/273.15)**0.5
-    df["SnowHeight"+str(sensor_level)]*=df['tr']
+    Tk=df['AirTemp'+str(sensor_level)].astype(float)+273.15
+    Tr=(Tk/273.15)**0.5
+    df["SnowHeight"+str(sensor_level)]*=Tr
 
 #%% clean check initial snow height data
 
@@ -127,6 +129,11 @@ if site=='DY2':
 print(z1,z2)
 
 
+#%%
+df.SnowHeight1=z1-df.SnowHeight1
+df.SnowHeight2=z2-df.SnowHeight2
+
+
 #%% filer impossible height values
 
 # df.SnowHeight1[df.SnowHeight1>8]=np.nan
@@ -139,10 +146,6 @@ for i,sensor_level in enumerate(range(1,3)):
     
 plt.plot(z1-df.SnowHeight1)
 plt.plot(z2-df.SnowHeight2)
-#%%
-df.SnowHeight1=z1-df.SnowHeight1
-df.SnowHeight2=z2-df.SnowHeight2
-
 
 # t0=datetime(df.year[v[0][0]], df.month[v[0][0]],df.day[v[0][0]]-1,df.hour[v[0][0]])
 # t1=datetime(df.year[v[0][0]], df.month[v[0][0]],df.day[v[0][0]],df.hour[v[0][0]])
@@ -211,6 +214,143 @@ plt.close()
 for i,sensor_level in enumerate(range(1,3)):
     plt.plot(df["SnowHeight"+str(sensor_level)],'.')
 
-#%%
+#%% copy to another dataframe to avoid needing to rerun the above
+aws=df
 
-df.to_csv(base_path+'/L0_modified/DY2_all_years_L0_modified.csv')
+##%%
+
+
+##%% read observed instrument height data
+
+obs_df = pd.read_excel('/Users/jason/Dropbox/GCNet_SWVF/ancil/GCNet_maintenance.xlsx',sheet_name="DYE-2",engine='openpyxl')
+# print(obs_df)
+
+obs_df["Date"]=pd.to_datetime(obs_df['Date (dd-mm-yyyy HH:MM)'])
+#obs_df.index = pd.to_datetime(obs_df.time)
+
+
+plt.close()
+
+n_rows=2
+fig, ax = plt.subplots(n_rows,1,figsize=(16,10))
+
+case_id=0
+print('profile instrument separation before dz',obs_df['W2 after (cm)'][case_id]-obs_df['W1 after (cm)'][case_id])
+print('profile instrument separation after dz',obs_df['W2 after (cm)'][case_id]-obs_df['W1 after (cm)'][case_id])
+profile_dz=(obs_df['W2 after (cm)'][case_id]-obs_df['W1 after (cm)'][case_id])/100
+
+for i,sensor_level in enumerate(range(1,3)):
+    aws['THW_z'+str(sensor_level)]=obs_df['W'+str(sensor_level)+' before (cm)'][0]/100-aws["SnowHeight"+str(sensor_level)]
+    if i:aws['THW_z'+str(sensor_level)]+=profile_dz
+    cc=0
+    ax[cc].plot(df["SnowHeight"+str(sensor_level)],'.',label='snow height '+str(sensor_level))
+    ax[cc].set_ylabel('surface height, m', color='k')
+    ax[cc].set_xticklabels([])
+    ax[cc].legend()
+    cc=1
+    ax[cc].plot(aws['THW_z'+str(sensor_level)],'.',label='instrument height '+str(sensor_level))
+    ax[cc].set_ylabel('instrument height, m', color='k')
+    mult=1
+    ax[cc].legend(prop={'size': font_size*mult})
+    # ax[cc].set_ylim(np.nanmin(y),np.nanmax(y))     
+    ax[cc].get_xaxis().set_visible(True)
+    ax[cc].legend(prop={'size': font_size})
+    ax[cc].legend()
+    # ax[cc].set_xlim(t0,t1)
+
+plt.setp(ax[1].xaxis.get_majorticklabels(), rotation=90,ha='center' )
+fig.tight_layout(pad=1.0)
+plt.show()
+#%% adjust for mast extensions and profile level adjustments
+
+from datetime import datetime
+
+# x=obs_df["Date"][1].strftime('%d-%m-%y')
+# x=pd.Timestamp(obs_df["Date"][1])
+
+case_id=4 # 1998 visit, noted in field notes, tower extension AND change in profile separation
+print("case",obs_df["Date"][case_id].strftime('%d-%m-%y'))
+print("wind z 1 before",obs_df['W1 before (cm)'][case_id]/100)
+print("wind z 1 after",obs_df['W1 after (cm)'][case_id]/100)
+print('profile instrument separation dz',obs_df['W2 after (cm)'][case_id]-obs_df['W1 after (cm)'][case_id])
+
+tx=obs_df["Date"][case_id].to_pydatetime().date()
+dz=np.zeros(2)
+for i,sensor_level in enumerate(range(1,3)):
+    dz[i]=np.nanmean(obs_df['W'+str(sensor_level)+' after (cm)'][case_id]/100-aws['THW_z'+str(sensor_level)][aws.date.dt.date==tx])
+    print('dz',1,dz[i])
+
+
+plt.close()
+
+n_rows=2
+fig, ax = plt.subplots(n_rows,1,figsize=(16,10))
+
+for i,sensor_level in enumerate(range(1,3)):
+    aws['THW_z'+str(sensor_level)]=obs_df['W'+str(sensor_level)+' before (cm)'][0]/100-aws["SnowHeight"+str(sensor_level)]
+    cc=0
+    ax[cc].plot(df["SnowHeight"+str(sensor_level)],'.',label='snow height '+str(sensor_level))
+    ax[cc].set_ylabel('snow height, m', color='k')
+    ax[cc].set_xticklabels([])
+    ax[cc].legend()
+
+    cc=1
+    aws['THW_z'+str(sensor_level)][aws.date.dt.date>tx]+=dz[i]
+    ax[cc].plot(aws['THW_z'+str(sensor_level)],'.',label='instrument height '+str(sensor_level))
+    ax[cc].set_ylabel('instrument height, m', color='k')
+    mult=1
+    ax[cc].legend(prop={'size': font_size*mult})
+    # ax[cc].set_ylim(np.nanmin(y),np.nanmax(y))     
+    ax[cc].get_xaxis().set_visible(True)
+    ax[cc].legend(prop={'size': font_size})
+    # ax[cc].set_xlim(t0,t1)
+    ax[cc].legend()
+
+plt.setp(ax[1].xaxis.get_majorticklabels(), rotation=90,ha='center' )
+fig.tight_layout(pad=1.0)
+
+
+
+#%% verify
+plt.close()
+
+n_rows=3
+fig, ax = plt.subplots(n_rows,1,figsize=(16,14))
+
+case_id=5 # 1999 visit, noted in field notes, no change in instrument heights
+tx=obs_df["Date"][case_id].to_pydatetime().date()
+print(tx)
+
+for i,sensor_level in enumerate(range(1,3)):
+    cc=0
+    ax[cc].plot(df["SnowHeight"+str(sensor_level)],'.',label='snow height '+str(sensor_level))
+    ax[cc].set_ylabel('snow height, m', color='k')
+    ax[cc].set_xticklabels([])
+    ax[cc].legend()
+
+    cc=1
+    ax[cc].plot(aws['THW_z'+str(sensor_level)],'.',label='instrument height '+str(sensor_level))
+    ax[cc].set_ylabel('instrument height, m', color='k')
+    mult=1
+    ax[cc].legend(prop={'size': font_size*mult})
+    # ax[cc].set_ylim(np.nanmin(y),np.nanmax(y))     
+    ax[cc].legend(prop={'size': font_size})
+    ax[cc].set_xticklabels([])
+    # ax[cc].set_xlim(t0,t1)
+    ax[cc].legend()
+
+    t1=obs_df['W'+str(sensor_level)+' before (cm)'][case_id]
+    t2=aws['THW_z'+str(sensor_level)][aws.date.dt.date==tx][0]
+    ax[cc].text(tx,aws['THW_z'+str(sensor_level)][aws.date.dt.date==tx][0],"{:.2f}".format(t1)+'\n'+"{:.2f}".format(t2),rotation=45)
+    ax[cc].text(tx,0,str(tx),rotation=0)
+
+cc=2
+ax[cc].set_ylabel('profile separation, m', color='k')
+ax[cc].plot(aws['THW_z'+str(2)]-aws['THW_z'+str(1)],'.',label='dz')
+ax[cc].legend()
+ax[cc].get_xaxis().set_visible(True)
+
+plt.setp(ax[1].xaxis.get_majorticklabels(), rotation=90,ha='center' )
+fig.tight_layout(pad=1.0)
+#%% write out dataframe
+# aws.to_csv(base_path+'/L0_modified/DY2_all_years_L0_modified.csv')
--- AWS_L0_JEB_filter_outliers.py
+++ AWS_L0_JEB_filter_outliers.py
@@ -15,7 +15,9 @@ import numpy as np
 import os
 from numpy.polynomial.polynomial import polyfit
 from datetime import datetime
-
+import matplotlib.dates as mdates
+from matplotlib.pyplot import figure
+import matplotlib
 # -------------------------------- chdir
 if os.getlogin() == 'Maiken':
     base_path = '/Users/Maiken/OneDrive/PUK/GCNet_SWVF/'
@@ -39,14 +41,62 @@ plt.rcParams["font.size"] = font_size
 df=pd.read_csv(base_path+'/L0_modified/DY2_all_years_L0_modified.csv')
 df = df.set_index('time')
 
+df.columns
+
+df['decimal_time']=df.year+df.JulianDay/366.
+
+# plt.plot(df["WindSpeed1"])
+# plt.gcf().autofmt_xdate()
+
 #%%
 
+n_rows=2
+fig, ax = plt.subplots(n_rows,1,figsize=(10,18))
+
+# from datetime import datetime
 plt.close()
 
-for i,sensor_level in enumerate(range(1,3)):
-    plt.plot(df["SnowHeight"+str(sensor_level)],'.')
+t0=datetime(1996, 5, 25,21) ; t1=datetime(1997, 8, 24)
 
-dates=['1996-05-25',
-       '1996-06-25']
-min_z=['0.01',
-       '0.2']
+# ax[cc].plot(df.WindSpeed1[t0:t1])
+
+cc=0
+for i,sensor_level in enumerate([1,2]):
+    print(i)
+    # plt.plot(df["SnowHeight"+str(sensor_level+1)][t0:t1])
+    plt.plot(df.decimal_time,df["SnowHeight"+str(sensor_level)])
+
+# ax.set_xlim(t0,t1)
+# ax.xaxis.set_major_locator(mdates.DayLocator(interval=1000))   #to get a tick every 15 minutes
+# ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+# xcolor='darkblue'
+# ax.xaxis.label.set_color(xcolor)
+# ax.tick_params(axis='x', colors=xcolor)
+mult=0.8
+# ax.text(-0.15,0.0, "day of\nAug.'21",transform=ax.transAxes, fontsize=font_size*mult,
+#     verticalalignment='top',rotation=0,color=xcolor, rotation_mode="anchor")  
+
+# ticks = ax.get_xticks()
+# labels = ax.get_xticklabels()
+# n = len(ticks) // 10  # Show 10 ticks.
+# ax.set_xticks(ticks[::n])
+# ax.set_xticklabels(labels[::n])
+
+# plt.setp(ax.xaxis.get_majorticklabels(), rotation=90,ha='center' )
+
+# ax[cc].set_xlim(t0,t1)
+# ax[cc].xaxis.set_major_locator(mdates.DayLocator(interval=100)) 
+# ax[cc].xaxis.set_major_formatter(mdates.DateFormatter('%d'))
+# xcolor='darkblue'
+# ax[cc].xaxis.label.set_color(xcolor)
+# ax[cc].tick_params(axis='x', colors=xcolor)
+# mult=0.8
+# ax[cc].text(-0.15,0.0, "day of\nAug.'21",transform=ax[cc].transAxes, fontsize=font_size*mult,
+#     verticalalignment='top',rotation=0,color=xcolor, rotation_mode="anchor")  
+# plt.setp(ax[cc].xaxis.get_majorticklabels(), rotation=90,ha='center' )
+
+plt.show()
+# dates=['1996-05-25',
+#        '1996-06-25']
+# min_z=['0.01',
+#        '0.2']
